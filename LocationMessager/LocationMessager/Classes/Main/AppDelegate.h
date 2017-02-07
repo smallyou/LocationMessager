0dbe49fda76811883affed4cab78174b52e578d7
23 <a23@23deiMac.local>
2017-02-07 14:01:22 +0800

    初始化项目

---
+++ LocationMessager/LocationMessager/Classes/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationMessager
+//
+//  Created by 23 on 2017/2/7.
+//  Copyright © 2017年 23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationMessager/LocationMessager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationMessager
+//
+//  Created by 23 on 2017/2/7.
+//  Copyright © 2017年 23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
